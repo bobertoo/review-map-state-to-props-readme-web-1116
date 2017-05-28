174d216b8a3d3685ff71d68165eebe354c20fc20
Vinny Martinez <vinny@vinnymartinez.com>
2017-05-28 14:44:18 -0400

    user and item counter working

--- src/App.js
+++ src/App.js
@@ -3,29 +3,31 @@ import './App.css';
 import { connect } from 'react-redux';
 
 class App extends Component {
-  handleOnClickItems(){
+  handleOnClickItems() {
     this.props.store.dispatch({type: 'GET_COUNT_OF_ITEMS'})
   }
-  handleOnClickUsers(){
+
+  handleOnClickUsers() {
     this.props.store.dispatch({type: 'GET_COUNT_OF_USERS'})
   }
+
   render() {
-    debugger;
     return (
       <div className="App">
           <button onClick={this.handleOnClickItems.bind(this)}>Click to change items count</button>
           <button onClick={this.handleOnClickUsers.bind(this)}>Click to change user count</button>
           <p> {this.props.items.length}</p>
+          <p> {this.props.users.length}</p>
       </div>
     );
   }
 }
 
-const connectedComponent = connect(mapStateToProps)(App)
-
-function mapStateToProps(state){
-  debugger;
-  return {items: state.items}
+function mapStateToProps(state) {
+  return {
+    items: state.items,
+    users: state.users
+  }
 }
 
-export default connectedComponent;
+export default connect(mapStateToProps)(App);
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,11 +1,11 @@
 export default function shoppingListItemReducer(state = {items: [], users: ['initial user']}, action) {
   switch(action.type) {
     case 'GET_COUNT_OF_ITEMS':
-    let items = state.items.concat(state.items.length + 1)
-    return Object.assign({}, state, {items: items})
+      let items = state.items.concat(state.items.length + 1);
+      return Object.assign({}, state, {items: items});
     case 'GET_COUNT_OF_USERS':
-      let users = state.users.concat(state.users.length + 1)
-      return Object.assign({}, state, {users: users})
+      let users = state.users.concat(state.users.length + 1);
+      return Object.assign({}, state, {users: users});
     default:
       return state;
   }
